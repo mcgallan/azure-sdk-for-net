6e3dfde6a54c81f9664e6eaf5fe7dbfca5deaf7b
mcgallan <1186003288@qq.com>
2023-07-28 15:02:01 +0800

    Create OperationalInsightsTableData.cs

---
+++ sdk/operationalinsights/Azure.ResourceManager.OperationalInsights/src/Custom/Models/OperationalInsightsTableData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Azure.ResourceManager.OperationalInsights.Custom.Models
+{
+    internal class OperationalInsightsTableData
+    {
+    }
+}
