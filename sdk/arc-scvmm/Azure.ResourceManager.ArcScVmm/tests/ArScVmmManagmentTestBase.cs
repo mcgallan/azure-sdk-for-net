6fe680d14d46fe6cf21fa0573a16b1fc99cca291
mcgallan <1186003288@qq.com>
2023-11-09 15:14:24 +0800

    update

---
+++ sdk/arc-scvmm/Azure.ResourceManager.ArcScVmm/tests/ArScVmmManagmentTestBase.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core;
+using Azure.Core.TestFramework;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.TestFramework;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace Azure.ResourceManager.ArcScVmm.Tests
+{
+    public class ArScVmmManagmentTestBase : ManagementRecordedTestBase<ArcScVmmManagementTestEnvironment>
+    {
+        protected AzureLocation DefaultLocation => AzureLocation.EastUS;
+        protected ArmClient Client { get; private set; }
+        protected SubscriptionResource DefaultSubscription { get; private set; }
+        public ArScVmmManagmentTestBase(bool isAsync) : base(isAsync)
+        {
+        }
+
+        public ArScVmmManagmentTestBase(bool isAsync, RecordedTestMode mode) : base(isAsync, mode)
+        {
+        }
+
+        protected ArScVmmManagmentTestBase(bool isAsync, ResourceType resourceType, string apiVersion, RecordedTestMode? mode = null)
+            : base(isAsync, resourceType, apiVersion, mode)
+        {
+        }
+
+        [SetUp]
+        public async Task CreateCommonClient()
+        {
+            Client = GetArmClient();
+            DefaultSubscription = await Client.GetDefaultSubscriptionAsync().ConfigureAwait(false);
+        }
+
+        protected async Task<ResourceGroupResource> CreateResourceGroupAsync()
+        {
+            var resourceGroupName = Recording.GenerateAssetName("testRG-");
+            var rgOp = await DefaultSubscription.GetResourceGroups().CreateOrUpdateAsync(
+                WaitUntil.Completed,
+                resourceGroupName,
+                new ResourceGroupData(DefaultLocation)
+                {
+                    Tags =
+                    {
+                        { "test", "env" }
+                    }
+                });
+            return rgOp.Value;
+        }
+    }
+}
---
+++ sdk/arc-scvmm/Azure.ResourceManager.ArcScVmm/tests/Helpers/ResourceDataHelper.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.ResourceManager.ArcScVmm.Models;
+using Azure.ResourceManager.Models;
+using NuGet.Frameworks;
+using NUnit.Framework;
+
+namespace Azure.ResourceManager.ArcScVmm.Tests.Helpers
+{
+    public static class ResourceDataHelper
+    {
+        // Temporary solution since the one in Azure.ResourceManager.Compute is internal
+        public static IDictionary<string, string> ReplaceWith(this IDictionary<string, string> dest, IDictionary<string, string> src)
+        {
+            dest.Clear();
+            foreach (var kv in src)
+            {
+                dest.Add(kv);
+            }
+
+            return dest;
+        }
+
+        public static void AssertTrackedResource(TrackedResourceData r1, TrackedResourceData r2)
+        {
+            Assert.AreEqual(r1.Name, r2.Name);
+            Assert.AreEqual(r1.Id, r2.Id);
+            Assert.AreEqual(r1.ResourceType, r2.ResourceType);
+            Assert.AreEqual(r1.Location, r2.Location);
+            Assert.AreEqual(r1.Tags, r2.Tags);
+        }
+        public static void AssertResource(ResourceData r1, ResourceData r2)
+        {
+            Assert.AreEqual(r1.Name, r2.Name);
+            Assert.AreEqual(r1.Id, r2.Id);
+            Assert.AreEqual(r1.ResourceType, r2.ResourceType);
+        }
+
+        #region InnventoryItemData
+        public static InventoryItemData GetInventoryItemData()
+        {
+            return new InventoryItemData(InventoryType.Cloud);
+        }
+        public static void AssetInnventoryItemData(InventoryItemData data1, InventoryItemData data2)
+        {
+            AssertResource(data1, data2);
+            Assert.AreEqual(data1.InventoryType, data2.InventoryType);
+            Assert.AreEqual(data1.ProvisioningState, data2.ProvisioningState);
+        }
+        #endregion
+    }
+}
