d62024e39586964d483b12baa21fa33dfba58b3e
mcgallan <1186003288@qq.com>
2024-11-26 13:35:23 +0800

    Create MachineLearningRegistryDataVersionResourceTests.cs

---
+++ sdk/machinelearningservices/Azure.ResourceManager.MachineLearning/tests/Scenario/MachineLearningRegistryDataVersionResourceTests.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Core.TestFramework;
+using Azure.ResourceManager.Compute;
+using Azure.ResourceManager.Compute.Models;
+using Azure.ResourceManager.KeyVault;
+using Azure.ResourceManager.MachineLearning;
+using Azure.ResourceManager.MachineLearning.Models;
+using Azure.ResourceManager.Models;
+using Azure.ResourceManager.Network;
+using Azure.ResourceManager.Resources;
+using NUnit.Framework;
+using Azure.ResourceManager.KeyVault.Models;
+
+namespace Azure.ResourceManager.MachineLearning.Tests
+{
+    public class MachineLearningRegistryDataVersionResourceTests : MachineLearningTestBase
+    {
+        public MachineLearningRegistryDataVersionResourceTests(bool isAsync)
+        : base(isAsync, RecordedTestMode.Record)
+        {}
+
+        [Test]
+        public async Task GetVersionResource()
+        {
+            var subscriptionId = "4d042dc6-fe17-4698-a23f-ec6a8d1e98f4";
+            var resourceGroupName = "deleteme1125";
+            var registryName = "registrytest1125";
+            var dataName = "datatest";
+            var machineLearningRegistryResourceId =
+            MachineLearningRegistryResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, registryName);
+            var machineLearningRegistry = Client.GetMachineLearningRegistryResource(machineLearningRegistryResourceId);
+
+            var dataCollection = machineLearningRegistry.GetMachineLearningRegistryDataContainers();
+            var property = new MachineLearningDataContainerProperties(MachineLearningDataType.UriFolder);
+            var datainput = new MachineLearningDataContainerData(property);
+            var dataResource = await dataCollection.CreateOrUpdateAsync(WaitUntil.Completed, dataName, datainput);
+
+            var versions = dataResource.Value.GetMachineLearningRegistryDataVersions();
+            var versionproperties = new MachineLearningDataVersionProperties(new Uri("data"));
+            var versiondata = new MachineLearningDataVersionData(versionproperties);
+            var latestVersion = await versions.CreateOrUpdateAsync(WaitUntil.Completed, "v11.25", versiondata);
+        }
+    }
+}
